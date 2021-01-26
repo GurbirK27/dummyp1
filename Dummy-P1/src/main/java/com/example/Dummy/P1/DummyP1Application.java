bc985bdd8edcce05b846902dfbdabda1b5192faf
gurbir <abc@test.com>
2021-01-26 04:32:06 -0500

    first commit

---
+++ Dummy-P1/src/main/java/com/example/Dummy/P1/DummyP1Application.java
@@ -0,0 +1,13 @@
+package com.example.Dummy.P1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DummyP1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DummyP1Application.class, args);
+	}
+
+}
